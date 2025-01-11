19565dbb18d9d924dff38c05947e9f5d30e2438e
Kikemm11 <Kikemaldonado11@gmail.com>
2025-01-10 23:10:27 -0400

    Add Database.dart. Add sqflite and provider dependencies. Update 'com.android.application' version to 8.2.2

---
+++ lib/database/Database.dart
@@ -0,0 +1,29 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+// Class definition
+class DBProvider {
+  DBProvider._();
+
+  static final DBProvider db = DBProvider._();
+  static Database? _database;
+
+  // Singleton Database instance
+  Future<Database?> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await initDB();
+    return _database;
+  }
+
+  // DB initializer
+  initDB() async {
+    return await openDatabase(
+        join(await getDatabasesPath(), 'enlanados_app_mobile.db'),
+        onCreate: (db, version) async {},
+        version: 1
+    );
+  }
+}
